02b8784850ed5b67ebd963a1a4c019bc137a7b73
Tenplant <tenplant1203@gmail.com>
2025-04-20 17:37:47 +0900

    Ex05:AとBのリファクタリング2

--- Ex05/A/Point2D.java
+++ Ex05/A/Point2D.java
@@ -2,6 +2,11 @@ public class Point2D {
   private double x;
   private double y;
 
+  public Point2D() {
+    this.x = 0;
+    this.y = 0;
+  }
+
   public Point2D(double x, double y) {
     this.x = x;
     this.y = y;
