77e7b29d611dff7bb8a1afda2d6307e18ec0ea6e
Tenplant <tenplant1203@gmail.com>
2025-04-22 15:34:53 +0900

    Ex06: A問題の解答コードを追加

---
+++ Ex06/B/Point.java
@@ -0,0 +1,21 @@
+class Quadrangle /* your code */ {
+  protected Point[] P;
+
+  public Quadrangle(Point p1, Point p2, Point p3, Point p4) {
+    /*
+     * your code
+     */
+  }
+
+  public void print() {
+    /*
+     * your code
+     */
+  }
+
+  public void move(int dx, int dy) {
+    /*
+     * your code
+     */
+  }
+}
