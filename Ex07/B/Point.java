9ec619ab720aa092eaa435775b57707eb82b563f
Tenplant <tenplant1203@gmail.com>
2025-04-30 10:39:01 +0900

    Ex07:B問題のコードを追加

---
+++ Ex07/B/Point.java
@@ -0,0 +1,28 @@
+public class Point implements Relatable {
+  private int x;
+  private int y;
+
+  public Point(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
+
+  public boolean isSmallerThan(Relatable other) {
+    if (this.length() < other.length()) {
+      return true;
+    }
+    return false;
+  }
+
+  public double length() {
+    return Math.sqrt(x * x + y * y);
+  }
+}
---
+++ Ex07/B/Relatable.java
@@ -0,0 +1,5 @@
+public interface Relatable {
+  double length();
+
+  public boolean isSmallerThan(Relatable other);
+}
