d11746dcd98f672aca91a2c17d3a077b2379c7f8
Tenplant <tenplant1203@gmail.com>
2025-04-30 09:59:54 +0900

    Ex06:Point classのリファクタリング

--- Ex06/B/Point.java
+++ Ex06/B/Point.java
@@ -1,20 +1,29 @@
-class Point {
+public class Point {
+  private int x;
+  private int y;
 
-    private int x;
-    private int y;
+  public Point(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
 
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
+  public void move(int dx, int dy) {
+    this.x += dx;
+    this.y += dy;
+  }
 
-    public void move(int dx, int dy) {
-        this.x += dx;
-        this.y += dy;
-    }
+  @Override
+  public String toString() {
+    return "(" + this.x + ", " + this.y + ")";
+  }
 
-    @Override
-    public String toString() {
-        return "(" + this.x + ", " + this.y + ")";
-    }
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null || getClass() != obj.getClass())
+      return false;
+    Point other = (Point) obj;
+    return this.x == other.x && this.y == other.y;
+  }
 }
--- Ex06/C/Point.java
+++ Ex06/C/Point.java
@@ -31,4 +31,19 @@ public class Point {
   public void print() {
     System.out.printf("(%d, %d)", this.x, this.y);
   }
+
+  @Override
+  public String toString() {
+    return "(" + this.x + ", " + this.y + ")";
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null || getClass() != obj.getClass())
+      return false;
+    Point other = (Point) obj;
+    return this.x == other.x && this.y == other.y;
+  }
 }
