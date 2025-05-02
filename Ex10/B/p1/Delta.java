c7eb8e0d7317506ff898ebb270f5987b4b7341d4
Tenplant <tenplant1203@gmail.com>
2025-05-02 18:14:25 +0900

    Ex10:B問題のコードを追加

---
+++ Ex10/B/p1/Delta.java
@@ -0,0 +1,20 @@
+package B.p1;
+
+import B.Alpha;
+import B.Gamma;
+
+public class Delta {
+  Alpha alpha = new Alpha();
+  Beta beta = new Beta();
+  Gamma gamma = new Gamma();
+
+  public Delta() {
+    alpha.show(gamma);
+    beta.show(gamma);
+    gamma.show(alpha);
+  }
+
+  public static void main(String[] args) {
+    new Delta();
+  }
+}
