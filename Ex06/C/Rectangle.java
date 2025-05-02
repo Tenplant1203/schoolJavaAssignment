c7b6133917f861f692a709235cb9fe42c4649ec7
Tenplant <tenplant1203@gmail.com>
2025-05-02 15:41:19 +0900

    Ex06:C問題のコードを修正

--- Ex06/C/Rectangle.java
+++ Ex06/C/Rectangle.java
@@ -1,29 +1,5 @@
-public class Rectangle extends Shape {
-  private Point[] points;
-
+public class Rectangle extends Parallelogram {
   public Rectangle(Point p1, Point p2) {
-    this.points = new Point[4];
-    this.points[0] = p1;
-    this.points[1] = new Point(p2.getX(), p1.getY());
-    this.points[2] = p2;
-    this.points[3] = new Point(p1.getX(), p2.getY());
-  }
-
-  @Override
-  public void move(int dx, int dy) {
-    for (Point point : this.points) {
-      point.move(dx, dy);
-    }
-  }
-
-  @Override
-  public void print() {
-    super.print();
-    for (int i = 0; i < 4; i++) {
-      points[i].print();
-      if (i < 3)
-        System.out.print("-");
-    }
-    System.out.println();
+    super(p1, p2, p2.getX() - p1.getX());
   }
 }
--- Ex06/C/Square.java
+++ Ex06/C/Square.java
@@ -1,29 +1,5 @@
-public class Square extends Shape {
-  private Point[] points;
-
+public class Square extends Rectangle {
   public Square(Point p1, int width) {
-    this.points = new Point[4];
-    this.points[0] = p1;
-    this.points[1] = new Point(p1.getX() + width, p1.getY());
-    this.points[2] = new Point(this.points[1].getX(), this.points[1].getY() + width);
-    this.points[3] = new Point(this.points[0].getX(), this.points[2].getY());
-  }
-
-  @Override
-  public void move(int dx, int dy) {
-    for (Point point : this.points) {
-      point.move(dx, dy);
-    }
-  }
-
-  @Override
-  public void print() {
-    super.print();
-    for (int i = 0; i < 4; i++) {
-      points[i].print();
-      if (i < 3)
-        System.out.print("-");
-    }
-    System.out.println();
+    super(p1, new Point(p1.getX() + width, p1.getY() + width));
   }
 }
